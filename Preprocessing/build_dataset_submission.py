983904cb5042d44cf50dd4339bff596e815b9d1e
Johannes Knappskog <johankna@stud.ntnu.no>
2026-03-25 13:06:06 +0100

    Added submission script

---
+++ Preprocessing/build_dataset_submission.py
@@ -0,0 +1,69 @@
+import os
+from pathlib import Path
+import pandas as pd
+
+root_dir = 'C:/Users/joh-k/OneDrive/Dokumenter/Fag_vaaren_26/TDT4265/Dataset/ODELIA2025/data'
+
+def build_dataset_submission(root_dir):
+    root = Path(root_dir)
+
+    test_data = []
+
+    for hospital_dir in root.iterdir():
+        if not hospital_dir.is_dir():
+            continue
+
+        # ✅ ONLY RSH
+        if hospital_dir.name != "RSH":
+            continue
+
+        print(f"\nProcessing TEST hospital: {hospital_dir.name}")
+
+        split_path = hospital_dir / "metadata_unilateral" / "split.csv"
+        df_split = pd.read_csv(split_path)
+
+        for _, row in df_split.iterrows():
+            uid = row["UID"]
+
+            data_root = hospital_dir / "data_unilateral"
+            breast_folders = [
+                f for f in data_root.iterdir()
+                if f.name.startswith(uid)
+            ]
+
+            for bf in breast_folders:
+                name = bf.name.lower()
+
+                if "left" in name:
+                    breast = "left"
+                elif "right" in name:
+                    breast = "right"
+                else:
+                    continue
+
+                nii_files_all = sorted(list(bf.glob("*.nii.gz")))
+
+                selected_files = [
+                    f for f in nii_files_all
+                    if not any(x in f.name.lower() for x in ["post_3", "post_4", "post_5"])
+                ]
+
+                if len(selected_files) == 0:
+                    continue
+
+                sample = {
+                    "image": [str(f) for f in selected_files],
+                    "id": f"{uid}_{breast}"  # ✅ REQUIRED FOR CSV
+                }
+
+                test_data.append(sample)
+
+    print(f"\nRSH Test samples: {len(test_data)}")
+
+    return test_data
+
+
+test_data = build_dataset_submission(root_dir)
+
+print(test_data[0])
+print(len(test_data))
